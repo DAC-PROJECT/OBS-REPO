526f31379a6afd3ea843f9fc1e20f6cc1933572d
PiyushKSingh23 <singhpiyush2309@gmail.com>
2020-12-28 22:02:33 +0530

    Back End User Authentication

--- backend/routes/booksRoutes.js
+++ backend/routes/booksRoutes.js
@@ -1,29 +1,14 @@
 import express from 'express'
-import asyncHandler from 'express-async-handler'
 const router=express.Router()
-import Book from '../models/bookModel.js'
+import {getBooks,getBookById} from '../controllers/bookController.js'
 
 
-//@desc Fetch all books
-//@route Get /api/books
-//@access public
-router.get('/',asyncHandler(async(req,res)=>{
-    const books = await Book.find({})
-    res.json(books);  
-}))
+
+router.route('/').get(getBooks)
+router.route('/:id').get(getBookById)
 
 
-//@desc Fetch single book
-//@route Get /api/books/:id
-//@access public
-router.get('/:id',asyncHandler(async(req,res)=>{
-  const book=await Book.findById(req.params.id)
-  if(book){
- res.json(book);
- }  else{
-     res.status(404)
-     throw new Error('Book not found')
- }
-}))
+
+
 
 export default router
--- backend/server.js
+++ backend/server.js
@@ -4,17 +4,21 @@ import colors from 'colors'
 import {notFound,errorHandler} from './middleware/errorMiddleware.js'
 import connectDb from './config/db.js'
 import bookRoutes from './routes/booksRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config()
 connectDb()
 const app=express()
  
+app.use(express.json())
+
 app.get('/',(req,res)=>{
     res.send('Api is Running');
 })
 
 
 app.use('/api/books',bookRoutes)
+app.use('/api/users',userRoutes) 
 
 app.use(notFound)
 
